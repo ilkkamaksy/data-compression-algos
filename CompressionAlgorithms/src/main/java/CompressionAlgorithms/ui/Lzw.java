9aa6eaf30528c4343e9f16df2490727761b0360f
ilkkamaksy <ijmakine@gmail.com>
2021-03-25 21:49:13 +0100

    LZW skeleton

---
+++ CompressionAlgorithms/src/main/java/CompressionAlgorithms/ui/Lzw.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CompressionAlgorithms.ui;
+
+/**
+ * The Lempel Ziv Welch algorithm 
+ */
+public class Lzw {
+    public String compress(String content) {
+        return "compressed";
+    }
+    
+    public String extract(String content) {
+        return "extracted";
+    }
+}
