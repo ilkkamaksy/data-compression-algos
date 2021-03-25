046b707d79c9e1a6d3416fa1ceeec0ba7918a9ea
ilkkamaksy <ijmakine@gmail.com>
2021-03-25 21:50:32 +0100

    moved lzw

--- CompressionAlgorithms/src/main/java/CompressionAlgorithms/domain/Lzw.java
+++ CompressionAlgorithms/src/main/java/CompressionAlgorithms/domain/Lzw.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package CompressionAlgorithms.ui;
+package CompressionAlgorithms.domain;
 
 /**
  * The Lempel Ziv Welch algorithm 
