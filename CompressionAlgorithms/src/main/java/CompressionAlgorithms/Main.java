664ee92cd74fe81f04001061ccd6695862fe6ca3
ilkkamaksy <ijmakine@gmail.com>
2021-04-30 00:02:50 +0200

    remove unused imports

--- CompressionAlgorithms/src/main/java/CompressionAlgorithms/Main.java
+++ CompressionAlgorithms/src/main/java/CompressionAlgorithms/Main.java
@@ -1,18 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package CompressionAlgorithms;
 
-import CompressionAlgorithms.domain.HuffmanCode;
-import CompressionAlgorithms.domain.HuffmanNode;
 import CompressionAlgorithms.ui.Launcher;
 
-/**
- *
- * @author ilkka
- */
 public class Main {
 
     /**
