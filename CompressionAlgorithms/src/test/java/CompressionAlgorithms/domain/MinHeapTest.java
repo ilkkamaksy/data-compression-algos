7b90c2c7753683451d0190ca8145f839682bf544
ilkkamaksy <ijmakine@gmail.com>
2021-05-01 14:12:12 +0200

    cleanup

--- CompressionAlgorithms/src/test/java/CompressionAlgorithms/domain/MinHeapTest.java
+++ CompressionAlgorithms/src/test/java/CompressionAlgorithms/domain/MinHeapTest.java
@@ -1,42 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package CompressionAlgorithms.domain;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author ilkka
- */
 public class MinHeapTest {
     
-    public MinHeapTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
     @Test
     public void elementsCanBeAdded() {
         MinHeap instance = new MinHeap();
