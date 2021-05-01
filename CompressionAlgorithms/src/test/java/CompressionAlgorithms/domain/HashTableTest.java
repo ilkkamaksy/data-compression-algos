96c73028811926d1727a9817aa21ed4bf788a7eb
ilkkamaksy <ijmakine@gmail.com>
2021-05-01 16:01:57 +0200

    cleanup

--- CompressionAlgorithms/src/test/java/CompressionAlgorithms/domain/HashTableTest.java
+++ CompressionAlgorithms/src/test/java/CompressionAlgorithms/domain/HashTableTest.java
@@ -1,43 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package CompressionAlgorithms.domain;
 
 import java.util.Random;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author ilkka
- */
 public class HashTableTest {
     
-    public HashTableTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
     @Test
     public void getNonPresentKeyReturnsNull() {
         HashTable instance = new HashTable();
