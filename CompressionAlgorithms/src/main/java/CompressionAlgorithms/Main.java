309c55992275e50222a775c3ba96eec00012d70f
ilkkamaksy <ijmakine@gmail.com>
2021-05-16 10:57:42 +0200

    cleanup

--- CompressionAlgorithms/src/main/java/CompressionAlgorithms/Main.java
+++ CompressionAlgorithms/src/main/java/CompressionAlgorithms/Main.java
@@ -10,7 +10,6 @@ public class Main {
      */
     public static void main(String[] args) {        
         
-        boolean testMode = false;
         for (String arg : args) {
             if (arg.equals("compare")) {
                 Compare.run();
---
+++ documentation/usermanual.md
@@ -0,0 +1,13 @@
+# User manual
+
+## Run the app
+
+Run the app with
+
+`./gradlew run`
+
+## Performance tests
+
+Run the performance tests with
+
+`./gradlew run --args="compare"`
